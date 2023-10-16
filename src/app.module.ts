2c75686676d3c625e3c0e8d6c201d8a4b313737f
raniaelouni <raniaelouni421@gmail.com>
2023-10-16 21:27:34 +0100

    modify noreply text

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
+import { MailService } from './mail/mail.service';
 
 @Module({
   imports: [MailModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,MailService],
 })
 export class AppModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,26 +5,26 @@ import { MailerService } from '@nestjs-modules/mailer';
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async mailForgotPassword(email: string, name: string) {
+  async mailNoRelpy(email: string, name: string) {
     let result;
     try {
       result = await this.mailerService.sendMail({
         to: email,
         from: 'no-reply@sfectoria.com',
-        subject: 'Test Mailer ✔',
-        text: 'welcome',
-        html: `<h1>welcome</h1><p>${name}</p>`,
+        subject: 'Thank You for Contacting Sfactoria',
+        text: 'This is a fallback text for email clients that do not support HTML.',
+        html: `<h1>Welcome</h1>
+              <p>${name},</p>
+              <p>Thank you for reaching out to Sfactoria. We appreciate your interest and the time you've taken to get in touch with us. Your message is important to us, and we are eager to assist you.</p>
+              <p>Please note that your email has been received, and we will respond as soon as possible.</p>`
       });
       console.log(result);
       console.log('==Result==');
       return result;
     }
-    
     catch (error) {
       console.log(error);
       console.log('==Error==');
     }
-
- 
   }
 }
