3ac90df572e1435555f4fea54f75613778aeb75e
khalil kraiem <khalilmedkraiem@gmail>
2023-10-18 18:26:57 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({optionsSuccessStatus:200,credentials:true, });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors()
   app.setGlobalPrefix('/api/v1');
   await app.listen(9000);
 }
